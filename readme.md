7f3fba851ba04e4eb70b8164058cdc000460ae09
Morgan <10958823+morganhvidt@users.noreply.github.com>
2021-04-23 15:46:07 +0800

    Added example plugin

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Reserved Stock Pro | Add-on - WooCommerce Subscription
+
+This is an example plugin that registers [WooCommerce Subscriptions](https://woocommerce.com/products/woocommerce-subscriptions/) product types with our [Reserved Stock Pro](https://puri.io/plugin/reserved-stock-pro-for-woocommerce/) plugin. 
+
+Once you activate this add-on plugin with Reserved Stock Pro subscription products with inventory can be added and reserved in customer carts.
+
+Have a look at our developer [documention](https://puri.io/docs/reserved-stock-pro/hooks/) for more information about the snippet and other features.
---
+++ reserved-stock-pro-addon-wc-subscriptions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name:       Reserved Stock Pro | Add-on - WooCommerce Subscription
+ * Plugin URI:        https://puri.io/plugin/reserve-stock-pro-for-woocommerce/
+ * Description:       Add-on enables simple subcription & variable subcription product types with Reserved Stock Pro.
+ * Version:           1.0.0
+ * Author:            Puri.io
+ * Author URI:        https://puri.io/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Add or remove specific product types from stock reservations.
+ */
+function puri_custom_rsp_supported_product_types( $supported_array ) {
+
+	array_push( $supported_array, 'subscription' ); // Simple Subscription Products.
+	array_push( $supported_array, 'variable-subscription' ); // Variable Subscription products.
+
+	return $supported_array;
+}
+add_filter( 'reserved_stock_pro_supported_product_types', 'puri_custom_rsp_supported_product_types', 10 );
